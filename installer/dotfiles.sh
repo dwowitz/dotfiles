df7589e4cc1b7b44fdb474c4458de45fee769354
Danny Berkowitz <dwowitz@gmail.com>
2017-04-28 19:10:53 -0500

    Create dependency check and pretty banner.

--- installer/dotfiles.sh
+++ installer/dotfiles.sh
@@ -7,14 +7,16 @@ dir="$HOME/dotfiles"            # dotfiles directory
 olddir="$HOME/dotfiles.$today"  # old dotfiles backup directory
 files=$(ls ${scriptdir}/../)    # list of files/folders to symlink
 dotignored=`cat ${scriptdir}/../.dotignore | egrep -v "^#"`
+deps=$(grep -v "^#" ${scriptdir}/dependencies.txt) # Read dependencies into variable.
 ##########
 
+# Source option parser and add options.
 . ${scriptdir}/../local/scripts/lib/sh/dynoptparse.sh
 options+=(["install"]="Install dotfiles to ${HOME}.")
 options+=(["update"]=" Pull latest files from git.")
 options+=(["backup"]=" Restore dotfiles from backup.")
 options+=(["restore"]="Restore dotfiles from backup. (WIP)")
-options+=(["depchk"]="Check for missing dependencies. (WIP)")
+options+=(["depchk"]=" Check for missing dependencies.")
 
 
 # Files in $scriptdir excluding $dotignored
@@ -24,12 +26,12 @@ dotfiles=$(comm -23 <(echo "$files" | sort) <(echo "$dotignored" | sort))
 backup()
 {
     # move any existing dotfiles in ~/ to $olddir directory
-    echo "Backing up current dotfiles to $olddir..."
-    mkdir -p $olddir
     for file in $dotfiles; do
         if [ -e ~/.$file ] && [ ! -L ~/.$file ]; then
             echo "Moving \".$file\" from \"$HOME\" to \"$olddir\""
-            mv $HOME/.$file $olddir/.$file
+            mkdir -p $olddir && mv $HOME/.$file $olddir/.$file
+        else
+            echo "Nothing to back up."
         fi
     done
 }
@@ -44,7 +46,7 @@ install()
     backup
     # symlink dotfiles to $HOME
     for file in $dotfiles; do
-        echo "Creating symlink to \"$file\" in home directory."
+        echo "Creating symlink \"${HOME}/.${file}\"."
         ln -sf $scriptdir/$file $HOME/.$file
     done
 
@@ -64,7 +66,7 @@ install()
 update()
 {
     if cd ${scriptdir}../ && git pull; then
-        install && exit 0
+        install
     else
         echo "Dotfiles could not be updated. I'm sorry :-("
         exit 1
@@ -73,7 +75,20 @@ update()
 
 depchk()
 {
-    echo "HELP! somebody needs to write me!"
+    for d in $deps; do
+        if which $d &> /dev/null; then
+            echo "Found $d in \$PATH."
+        else
+            echo "Make sure \"$d\" is in your \$PATH before installing dotfiles."
+            depsfailed+=1
+        fi
+    done
+
+    if [ $depsfailed -ge 1 ]; then
+        #echo "You have $depsfailed missing dependencies. Go fix it!!!"
+        exit 1
+    fi
 }
 
+. ${scriptdir}/dotfiles-ascii.sh
 parse_opts $@
